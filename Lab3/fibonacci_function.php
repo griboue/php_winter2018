78833824e97d289bcd4ed2ebd52f57b6d1b500b9
Jiahong Wu <jiahongwu123@gmail.com>
2018-04-03 17:05:01 -0400

    delete old one

--- Lab3/fibonacci_function.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/*
- * Please declare/define and use a Fibonacci function that has one optional parameter to determine
- * the number of numbers to be included in the Fibonacci sequence.
- */
--- Lab3/include/controller1.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-// This file should contain your first controller.
