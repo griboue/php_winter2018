fb7b94497ad8811288598f290f2bcc258d1c1220
Jiahong Wu <jiahongwu123@gmail.com>
2018-04-03 17:52:26 -0400

    finish controller exercise

--- Lab3_WU/front_controller.php
+++ Lab3_WU/front_controller.php
@@ -4,3 +4,13 @@
  * This file should contain your front controller and basic routing mechanism
  * in order to call the appropriate controller contained in the 'include' folder.
  */
+
+define('BASE_PATH', dirname(dirname(__FILE__)));
+
+if ($_SERVER['REQUEST_URI'] == BASE_PATH . DIRECTORY_SEPARATOR . 'Lab3_WU' . DIRECTORY_SEPARATOR . 'include' . DIRECTORY_SEPARATOR . 'controller1') {
+    include_once("include". DIRECTORY_SEPARATOR . "controller1.php");
+} elseif ($_SERVER['REQUEST_URI'] == BASE_PATH . DIRECTORY_SEPARATOR . 'Lab3_WU' . DIRECTORY_SEPARATOR . 'include' . DIRECTORY_SEPARATOR . 'controller2') {
+    include_once("include". DIRECTORY_SEPARATOR . "controller2.php");
+} else {
+    echo "<h1>page not found !!</h1>";
+}
--- Lab3_WU/include/controller1.php
+++ Lab3_WU/include/controller1.php
@@ -1,3 +1,5 @@
 <?php
 
 // This file should contain your first controller.
+
+echo "<h1>You are now in the page of controller1</h1>";
