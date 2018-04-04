d9d11d78b0d5482c7a934e760d6c927d8a88165c
Andrew Caya <andrewscaya@yahoo.ca>
2018-04-04 09:48:51 -0400

    2018-04-04 AC: Adds backend and environment code examples.

---
+++ Lab4/database_pdo.php
@@ -0,0 +1,7 @@
+<?php
+
+/*
+ * Re-create the same database operations that you can find in the 'database_1.php' file,
+ * but by using the PDO classes.
+ * http://php.net/manual/en/book.pdo.php
+ */
