8f81a0ea455b800bf1fd309a8e75f82d0a86d12c
Derek Wu <1114110754@qq.com>
2018-04-04 03:09:08 -0400

    Lab3 finished

--- Lab3_WU/front_controller.php
+++ Lab3_WU/front_controller.php
@@ -4,13 +4,21 @@
  * This file should contain your front controller and basic routing mechanism
  * in order to call the appropriate controller contained in the 'include' folder.
  */
+$url = parse_url($_SERVER["REQUEST_URI"],PHP_URL_QUERY);
 
-define('BASE_PATH', dirname(dirname(__FILE__)));
-
-if ($_SERVER['REQUEST_URI'] == BASE_PATH . DIRECTORY_SEPARATOR . 'Lab3_WU' . DIRECTORY_SEPARATOR . 'include' . DIRECTORY_SEPARATOR . 'controller1') {
-    include_once("include". DIRECTORY_SEPARATOR . "controller1.php");
-} elseif ($_SERVER['REQUEST_URI'] == BASE_PATH . DIRECTORY_SEPARATOR . 'Lab3_WU' . DIRECTORY_SEPARATOR . 'include' . DIRECTORY_SEPARATOR . 'controller2') {
-    include_once("include". DIRECTORY_SEPARATOR . "controller2.php");
-} else {
-    echo "<h1>page not found !!</h1>";
+switch ($url) {
+    // controller1
+    case 'controller1':
+        require 'include/controller1.php';
+        break;
+    // controller2
+    case 'controller2':
+        require 'include/controller2.php';
+        break;
+    // not found page
+    default:
+        echo "<h1>page not found !!</h1>";
+        break;
 }
+
+?>
--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 echo 'This is a message from test.php!' . PHP_EOL;
 
 function a($a, $b, $c = null, $d = null) {
@@ -9,4 +9,54 @@ function a($a, $b, $c = null, $d = null) {
     echo "d : " .$d . PHP_EOL;
 }
 
-a(1,2,3);
+a(1,2,3);*/
+
+$a = "a";
+$b = $a;
+echo "a : " . $a . PHP_EOL;
+echo "b : " . $b . PHP_EOL;
+$b = "b";
+echo "a : " . $a . PHP_EOL;
+echo "b : " . $b . PHP_EOL;
+$a = "a2";
+echo "a : " . $a . PHP_EOL;
+echo "b : " . $b . PHP_EOL;
+
+$apple = "apple";
+$$apple = "\$apple";
+echo $apple.PHP_EOL;
+echo $$apple.PHP_EOL;
+
+echo (0==false) ? "0 == false" : "0 != false";
+echo PHP_EOL;
+
+echo (0===false) ? "0 === false" : "0 !== false";
+echo PHP_EOL;
+
+$a = "5"; $b = 5;
+echo $a <> $b ? "true" : "false";
+echo PHP_EOL;
+
+var_dump(~9);
+var_dump(DIRECTORY_SEPARATOR);
+var_dump(PATH_SEPARATOR);
+
+//file_put_contents("file.txt","content\ncontent");
+
+function swape(&$a, &$b){
+    $temp = $a;
+    $a = $b;
+    $b = $temp;
+}
+
+$a = 3; $b = 5;
+echo $a . " " . $b . PHP_EOL;
+swape($a, $b);
+echo $a . " " . $b . PHP_EOL;
+
+
+
+
+
+
+
