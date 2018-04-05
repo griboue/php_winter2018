c6eec5d9005069f5f1a2f695df826bb6fa75975a
Cédric Francis Jean Cligniez <cedric.cligniez@efrei.net>
2018-04-05 16:42:12 -0400

    message

--- Lab3/front_controller.php
+++ Lab3/front_controller.php
@@ -4,3 +4,21 @@
  * This file should contain your front controller and basic routing mechanism
  * in order to call the appropriate controller contained in the 'include' folder.
  */
+
+include "include/controller1.php";
+include "include/controller2.php";
+
+class Controller{
+
+    public function __construct()
+    {
+        echo "Choose a controller: ";
+        $input = readline();
+        if($input == "1")
+            $controller = new controller1();
+        else
+            $controller = new controller2();
+    }
+}
+
+$front_controller = new Controller();
